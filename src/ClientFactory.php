349c7e3b829a0cce2e4ffa2d4c05f2780ffb1e8b
Thomas Nabord <thomas.nabord@prestashop.com>
2022-06-09 17:45:17 +0200

    Update deps and add ClientFactory

--- src/ClientFactory.php
+++ src/ClientFactory.php
@@ -2,6 +2,31 @@
 
 namespace Prestashop\ModuleLibGuzzleAdapter;
 
+use Prestashop\ModuleLibGuzzleAdapter\Guzzle5\Client as Guzzle5Client;
+use Prestashop\ModuleLibGuzzleAdapter\Guzzle7\Client as Guzzle7Client;
+use PrestaShop\ModuleLibGuzzleAdapter\VersionDetection;
+use Psr\Http\Client\ClientInterface;
+
 class ClientFactory {
-    
+    /**
+     * @var VersionDetection
+     */
+    private $versionDetection;
+
+    public function __construct(VersionDetection $versionDetection = null)
+    {
+        $this->versionDetection = $versionDetection;
+    }
+
+    /**
+     * @param array<string, mixed> $config
+     */
+    public function getClient(array $config = []): ClientInterface
+    {
+        if ($this->versionDetection->getGuzzleMajorVersionNumber() >= 7) {
+            return Guzzle7Client::createWithConfig($config);
+        }
+
+        return Guzzle5Client::createWithConfig($config);
+    }
 }
